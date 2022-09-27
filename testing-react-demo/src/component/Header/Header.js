6ca0c619ae525b9c64cbf6fc123c2a1b3a262c77
Your Name <kalaitzidispaul@gmail.com>
2022-09-27 12:22:55 +0200

    created header component

---
+++ testing-react-demo/src/component/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.scss'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='wrapper'>
+        <div className='logo'>
+        <h1>Logo</h1>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
