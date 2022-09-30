c54a022a88d22508e89657631fcec07b3dd9a478
Your Name <kalaitzidispaul@gmail.com>
2022-09-30 14:28:10 +0200

    post reducer with tests

---
+++ testing-react-demo/src/reducers/posts/reducer.test.js
@@ -0,0 +1,18 @@
+import {types} from '../../actions/types'
+import postsReducer from './reducer'
+
+describe("Testing Posts reducer", () => {
+    it("Should return default state", () => {
+        const newState = postsReducer(undefined, {});
+        expect(newState).toEqual([])
+    });
+
+    it("Should return new state if receiving type", () => {
+        const posts =[{ title: "test"}, { title: "test"}, { title: "test"}]
+        const newState = postsReducer(undefined, {
+            type: types.GET_POSTS,
+            payload: posts
+        })
+        expect(newState).toEqual(posts)
+    })
+});
